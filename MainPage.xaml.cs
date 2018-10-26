495e8655d44f9932e9daf44e9802e2b5fc9e8567
jia0 <jia0@dentall.io>
2018-10-26 14:29:18 +0800

    init

---
+++ MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+
+namespace DoubleDetails
+{
+    public sealed partial class MainPage : Page
+    {
+        private IList<ParentClass> _parentList;
+        public IList<ParentClass> ParentList = new List<ParentClass>()
+        {
+            new ParentClass()
+            {
+                Title = "First",
+                Id = 'A',
+                Children = new List<ChildClass>()
+                {
+                    new ChildClass(){Name = "A1",Id = 1},
+                    new ChildClass(){Name = "A2",Id = 2},
+                    new ChildClass(){Name = "A3",Id = 3},
+                }
+            }
+        };
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+
+            ParentList.Add(new ParentClass()
+            {
+                Title = "Second",
+                Id = 'B',
+                Children = new List<ChildClass>()
+                {
+                    new ChildClass(){Name = "B1",Id = 1},
+                    new ChildClass(){Name = "B2",Id = 2},
+                    new ChildClass(){Name = "B3",Id = 3},
+                }
+            });
+
+            ParentList.Add(new ParentClass()
+            {
+                Title = "Third",
+                Id = 'C',
+                Children = new List<ChildClass>()
+                {
+                    new ChildClass(){Name = "C1",Id = 1},
+                    new ChildClass(){Name = "C2",Id = 2},
+                    new ChildClass(){Name = "C3",Id = 3},
+                }
+            });
+        }
+    }
+
+    public class ChildClass
+    {
+        public string Name;
+        public int Id;
+    }
+
+    public class ParentClass
+    {
+        public string Title;
+        public char Id;
+        public List<ChildClass> Children;
+    }
+}
